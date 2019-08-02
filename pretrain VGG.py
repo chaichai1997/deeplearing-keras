daf26a2af0e3dc9e7075111cebc5daef353f6e26
ChaiChai <1224816105@qq.com>
2019-08-02 22:02:01 +0800

    VGG-fast-feature-extract

--- pretrain VGG.py
+++ pretrain VGG.py
@@ -1,2 +1,152 @@
 # -*- coding: utf-8 -*-
 # author = "chaichai"
+from keras.applications import VGG16
+import os
+import numpy as np
+from keras.preprocessing.image import ImageDataGenerator
+from keras import models
+from keras import layers
+from keras import optimizers
+import matplotlib.pyplot as plt
+"""
+在小数据集上使用预训练的神经网络，使用VGG16
+_________________________________________________________________
+Layer (type)                 Output Shape              Param #   
+=================================================================
+input_1 (InputLayer)         (None, 150, 150, 3)       0         
+_________________________________________________________________
+block1_conv1 (Conv2D)        (None, 150, 150, 64)      1792      
+_________________________________________________________________
+block1_conv2 (Conv2D)        (None, 150, 150, 64)      36928     
+_________________________________________________________________
+block1_pool (MaxPooling2D)   (None, 75, 75, 64)        0         
+_________________________________________________________________
+block2_conv1 (Conv2D)        (None, 75, 75, 128)       73856     
+_________________________________________________________________
+block2_conv2 (Conv2D)        (None, 75, 75, 128)       147584    
+_________________________________________________________________
+block2_pool (MaxPooling2D)   (None, 37, 37, 128)       0         
+_________________________________________________________________
+block3_conv1 (Conv2D)        (None, 37, 37, 256)       295168    
+_________________________________________________________________
+block3_conv2 (Conv2D)        (None, 37, 37, 256)       590080    
+_________________________________________________________________
+block3_conv3 (Conv2D)        (None, 37, 37, 256)       590080    
+_________________________________________________________________
+block3_pool (MaxPooling2D)   (None, 18, 18, 256)       0         
+_________________________________________________________________
+block4_conv1 (Conv2D)        (None, 18, 18, 512)       1180160   
+_________________________________________________________________
+block4_conv2 (Conv2D)        (None, 18, 18, 512)       2359808   
+_________________________________________________________________
+block4_conv3 (Conv2D)        (None, 18, 18, 512)       2359808   
+_________________________________________________________________
+block4_pool (MaxPooling2D)   (None, 9, 9, 512)         0         
+_________________________________________________________________
+block5_conv1 (Conv2D)        (None, 9, 9, 512)         2359808   
+_________________________________________________________________
+block5_conv2 (Conv2D)        (None, 9, 9, 512)         2359808   
+_________________________________________________________________
+block5_conv3 (Conv2D)        (None, 9, 9, 512)         2359808   
+_________________________________________________________________
+block5_pool (MaxPooling2D)   (None, 4, 4, 512)         0         
+=================================================================
+Total params: 14,714,688
+Trainable params: 14,714,688
+Non-trainable params: 0
+_________________________________________________________________
+None
+"""
+
+"""
+VGG16(include_top=True,
+          weights='imagenet',  # 指定模型初始化的权重检查点
+          input_tensor=None,   # 指定模型最后是否包含密集连接分类器
+          input_shape=None,    # 输入到网络中的图像的张量
+          pooling=None,
+          classes=1000,
+          **kwargs):
+"""
+conv_base = VGG16(
+    weights='imagenet',
+    include_top=False,
+    input_shape=(150, 150, 3)
+)
+"""
+快速特征提取
+在自己的数据集上运行卷积基，将输出保存为硬盘中的numpy数组，然后用数组作为输入，输入到独立的密集连接分类器中。
+"""
+base_dir = 'E:/deep learning/code/kreastest/cat-data'
+train_dir = os.path.join(base_dir, 'train')
+validation_dir = os.path.join(base_dir, 'validation')
+test_dir = os.path.join(base_dir, 'test')
+datagen = ImageDataGenerator(rescale=1./255)
+batch_size = 20
+
+
+def extract_features(directory, sample_count):
+    features = np.zeros(shape=(sample_count, 4, 4, 512))
+    labels = np.zeros(shape=(sample_count))
+    generators = datagen.flow_from_directory(
+        directory,
+        target_size=(150, 150),
+        batch_size=batch_size,
+        class_mode='binary'
+    )
+    i = 0
+    for inputs_batch, labels_batch in generators:
+        features_batch = conv_base.predict(inputs_batch)
+        features[i * batch_size:(i + 1)*batch_size] = features_batch
+        labels[i * batch_size:(i + 1)*batch_size] = labels_batch
+        i += 1
+        if i * batch_size >= sample_count:
+            break   # 读取所有数据后终止循环
+    return features, labels
+
+
+# 最后输出特征图的形状为(4, 4, 512)
+train_features, train_labels = extract_features(train_dir, 2000)
+validation_features, validation_labels = extract_features(validation_dir,  1000)
+test_features, test_labels  = extract_features(test_dir, 1000)
+train_features = np.reshape(train_features, (2000, 4 * 4 * 512))
+validation_features = np.reshape(validation_features, (1000, 4 * 4 * 512))
+test_features = np.reshape(test_features, (1000, 4 * 4 * 512))
+
+# 定义并训练密集连接分类器
+model = models.Sequential()
+model.add(layers.Dense(256, activation='relu', input_dim=4 * 4 * 512))
+model.add(layers.Dropout(0.5))
+model.add(layers.Dense(1, activation='sigmoid'))
+model.compile(
+    optimizer=optimizers.RMSprop(le=2e-5),
+    loss='binary_crossentropy',
+    metrics=['acc']
+)
+history = model.fit(
+    train_features,
+    train_labels,
+    epochs=30,
+    batch_size=20,
+    validation_data=(validation_features, validation_labels)
+)
+
+# 绘制结果
+acc = history.history['acc']
+val_acc = history.history['val_acc']
+loss = history.history['loss']
+val_loss = history.history['val_loss']
+epochs = range(1, len(acc)+1)
+plt.plot(epochs, acc, 'bo', label='Training acc')
+plt.plot(epochs, val_acc, 'b', label='Validation acc')
+plt.title('Training and validation acc')
+plt.legend()
+plt.figure()
+plt.plot(epochs, loss, 'bo', label='Training loss')
+plt.plot(epochs, val_loss, 'b', label='Validation loss')
+plt.title('Training and validation loss')
+plt.legend()
+plt.show()
+
+
+
+
