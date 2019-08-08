43ddd81f8cbf8a08189fcf1a9765fb3a18ed9667
ChaiChai <1224816105@qq.com>
2019-08-08 10:46:11 +0800

    stacked-GRU

--- RNN- temperature-predict.py
+++ RNN- temperature-predict.py
@@ -131,18 +131,62 @@ def evaluate_naive_method():
 #     )
 
 
-# 循环网络
+# # 循环网络
+# model = Sequential()
+# model.add(layers.GRU(32, input_shape=(None, float_data.shape[-1])))
+# model.add(layers.Dense(1))
+# model.compile(optimizer=RMSprop(), loss='mae')
+# history = model.fit_generator(
+#     train_gen,
+#     steps_per_epoch=500,
+#     epochs=20,
+#     validation_data=val_gen,
+#     validation_steps=val_steps
+# )
+
+
+# 使用dropout正则化基于GRU的模型
+# model = Sequential()
+# model.add(layers.GRU(
+#     32,
+#     dropout=0.2,
+#     recurrent_dropout=0.2,
+#     input_shape=(None, float_data.shape[-1])))
+# model.add(layers.Dense(1))
+# model.compile(optimizer=RMSprop(), loss='mae')
+# history = model.fit_generator(
+#     train_gen,
+#     steps_per_epoch=500,
+#     epochs=40,
+#     validation_data=val_gen,
+#     validation_steps=val_steps
+# )
+
+# 循环层堆叠
 model = Sequential()
-model.add(layers.GRU(32, input_shape=(None, float_data.shape[-1])))
+model.add(layers.GRU(
+    32,
+    dropout=0.1,
+    recurrent_dropout=0.5,
+    return_sequences=True,
+    input_shape=(None, float_data.shape[-1])
+))
+model.add(layers.GRU(
+    64,
+    activation='relu',
+    dropout=0.1,
+    recurrent_dropout=0.5
+))
 model.add(layers.Dense(1))
 model.compile(optimizer=RMSprop(), loss='mae')
 history = model.fit_generator(
     train_gen,
     steps_per_epoch=500,
-    epochs=20,
+    epochs=40,
     validation_data=val_gen,
     validation_steps=val_steps
 )
+
 loss = history.history['loss']
 val_loss = history.history['val_loss']
 epochs = range(1, len(loss)+1)
