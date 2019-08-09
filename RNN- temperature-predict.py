2c276fae98aa2ed151b74489e1c2ea8908dae7ae
ChaiChai <1224816105@qq.com>
2019-08-09 13:24:45 +0800

    cnn1D-imdb

--- RNN- temperature-predict.py
+++ RNN- temperature-predict.py
@@ -189,16 +189,38 @@ def evaluate_naive_method():
 
 
 # 双向GRU
+# model = Sequential()
+# model.add(layers.Bidirectional(
+#     layers.GRU(32), input_shape=(None, float_data.shape[-1])
+# ))
+# model.add(layers.Dense(1))
+# model.compile(optimizer=RMSprop(), loss='mae')
+# history = model.fit_generator(
+#     train_gen,
+#     steps_per_epoch=500,
+#     epochs=40,
+#     validation_data=val_gen,
+#     validation_steps=val_steps
+# )
+
+
+# CNN
 model = Sequential()
-model.add(layers.Bidirectional(
-    layers.GRU(32), input_shape=(None, float_data.shape[-1])
-))
+model.add(layers.Conv1D(32, 5, activation='relu',
+                        input_shape=(None, float_data.shape[-1])))
+model.add(layers.MaxPooling1D(3))
+model.add(layers.Conv1D(32, 5, activation='relu'))
+model.add(layers.MaxPooling1D(3))
+model.add(layers.GlobalAveragePooling1D())
 model.add(layers.Dense(1))
-model.compile(optimizer=RMSprop(), loss='mae')
+model.compile(
+    optimizer=RMSprop(),
+    loss='mae'
+)
 history = model.fit_generator(
     train_gen,
     steps_per_epoch=500,
-    epochs=40,
+    epochs=20,
     validation_data=val_gen,
     validation_steps=val_steps
 )
