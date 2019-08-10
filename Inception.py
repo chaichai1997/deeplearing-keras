c66893462752a4f40db772da6862f8a46e069bc8
ChaiChai <1224816105@qq.com>
2019-08-10 12:26:02 +0800

    inception

---
+++ Inception.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+# author = "chaichai"
+from keras import layers
+import numpy as np
+
+x = np.random.randint(128, 3, 3, 3)
+
+branch_a = layers.Conv2D(128, 1, activation='relu', strides=2)(x)
+branch_b = layers.Conv2D(128, 1, activation='relu')(x)
+branch_b = layers.Conv2D(128, 3,activation='relu', strides=2)(branch_b)
+branch_c = layers.AveragePooling2D(3, strides=2)(x)
+branch_c = layers.Conv2D(128, 3, activation='relu')(branch_c)
+
+branch_d = layers.Conv2D(128, 1, activation='relu')(x)
+branch_d = layers.Conv2D(128, 3, activation='relu')(branch_d)
+branch_d = layers.Conv2D(128, 3, activation='relu', strides=2)(branch_d)
+
+out_put = layers.concatenate([branch_a, branch_b, branch_c, branch_d], axis=-1)
